cbbe1f30f3fe2c307a044b16b52333bb1c178063
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-21 23:50:01 +0530

    Episode - 14 - Shopping Cart - Home Component created fetch call also completed and map call on json data completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import Product from "../components/Product";
+import Spinner from "../components/Spinner";
+
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
 
-  return <div>Home</div>;
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const output = await fetch(API_URL);
+      const json = await output.json();
+      setPosts(json);
+    }
+    catch (error) {
+      console.log("Error Came");
+      setPosts([]);
+    }
+
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center">
+      {
+        loading ? <Spinner /> :
+          ((posts.length > 0) ?
+            (<div>
+              {
+                posts.map((post) => (
+                  <Product key={post.id} post={post} />
+                ))
+              }
+            </div>) :
+            (<div>
+              <p>No data Found</p>
+            </div >)
+          )
+      }
+    </div >
+  );
 };
 
 export default Home;
