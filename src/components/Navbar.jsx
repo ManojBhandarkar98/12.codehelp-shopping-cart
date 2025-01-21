469fc84d0aa34010182bc4eaae18a2af4acb6b6f
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-21 23:42:54 +0530

    Episode - 14 - Shopping Cart - Navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,26 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div>
+    <div className="flex flex-row justify-between">
+      <NavLink to="/">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGPyZMTAlzTIj_9Q7-uwiZ5xvJmBoapXPX2m2guMTa46Sl4sLalobYX9tL_p_gd61vGE0&usqp=CAU"
+                alt="img not show" 
+                className="h-20" />
+      </NavLink>
+      <div>
+        <NavLink to="/">
+          <p>Home</p>
+        </NavLink>
+        <NavLink to="/cart">
+        <div>
+        <FaShoppingCart />
+        </div>
+        </NavLink>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Navbar;
