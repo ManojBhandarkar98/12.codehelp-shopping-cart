a524ba22fc078bcf04455af9af485a3906e602e8
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-22 07:47:40 +0530

    Episode - 14 - Shopping Cart - Product Component created

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,23 @@
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ post }) => {
+  return (<div>
+    <div>
+      <p>{post.title}</p>
+    </div>
+
+    <div>
+      <p>{post.description}</p>
+    </div>
+    <div>
+      <img src={post.image} />
+    </div>
+    <div><p>Rs.{post.price}</p></div>
+    <button>
+      {
+        true ? <p>Remove Item</p> : <p>Add to Cart</p>
+      }
+    </button>
+    <br />
+  </div>);
 };
 
 export default Product;
