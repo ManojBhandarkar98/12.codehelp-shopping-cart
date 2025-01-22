193a2b5931e8e34e38451f5fdf4e745a31682d57
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-22 09:31:02 +0530

    Episode - 14 - Shopping Cart - CartItem Component Created

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,25 @@
-const CartItem = () => {
-  return <div>CartItem</div>;
+import { FcDeleteDatabase } from "react-icons/fc";
+
+const CartItem = ({item}) => {
+  return (<div>
+    <div>
+      <div>
+        <img src={item.image} />
+      </div>
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.description}</p>
+        <div>
+          <p>{item.price}</p>
+          <div>
+          <FcDeleteDatabase/>
+        </div>
+        </div>
+       
+      </div>
+
+    </div>
+  </div>);
 };
 
 export default CartItem;
